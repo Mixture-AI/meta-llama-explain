9ffb5fae86bc4ee7f34c6571d362303b7757909a
NAN ZHANG <30752758+NANWOOD@users.noreply.github.com>
2024-06-03 23:03:26 +0800

    Update llama/model_cn_comment.py
    Co-authored-by: LastWhisper <whuwkl@gmail.com>

--- llama/model_cn_comment.py
+++ llama/model_cn_comment.py
@@ -26,6 +26,7 @@ class ModelArgs:
     n_heads: int = 32
     n_kv_heads: Optional[int] = None
     vocab_size: int = -1  # 后续由 tokenizer 定义.
+    # TODO [NAN]: Add a doc for SwiGLU.
     multiple_of: int = 256  # 使 SwiGLU 隐藏层的尺寸为较大的 2 的幂的整数倍.
     ffn_dim_multiplier: Optional[float] = None
     norm_eps: float = 1e-5
